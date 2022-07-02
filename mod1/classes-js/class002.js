8e1c3ba124cb878fe75d80fcf732942ee1dc1cdf
BrantLauro <brantlauro@gmail.com>
2022-07-02 14:54:27 -0300

    :white_check_mark: class002 updated

--- mod1/classes-js/class002.js
+++ mod1/classes-js/class002.js
@@ -1,3 +1,18 @@
 var firstVariable = "This is my first variable in JavaScript";
 let secondVariable = true;
-const numberPi = 3.14;
+const numberPi = 3.14; //Essa variável é, na verdade, uma constante!
+
+/*
+   Esse comentário tem
+   múltiplas Linhas,
+   tudo que estiver
+   escrito aqui dentro, 
+   não será executado.
+*/
+// Isso é um comentário de linha
+
+let var1;
+let a = 0, b = 0;
+
+// talvez eu tenha passado na faculdade e esteja muito feliz =D ?
+
