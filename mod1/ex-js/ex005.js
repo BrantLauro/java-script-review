538119f3c44d1a4b7a1397b5049c0bac69d64a0a
BrantLauro <brantlauro@gmail.com>
2022-07-02 15:32:08 -0300

    :white_check_mark: ex005.js done

---
+++ mod1/ex-js/ex005.js
@@ -0,0 +1,10 @@
+
+// Aqui declarei uma variável boolean com o valor false
+let bol = false;
+
+// Aqui criei um condicional que fala que o valor é verdadeiro quando o valor de bol é verdadeiro e fala que o valor é falso quando o valor de bol é falso
+if (bol) {
+    console.log("The value is true");
+} else {
+    console.log("The value is false");
+}
