ab6408c9f38632ed54ea0ac964ce64ccf05bfdb1
BrantLauro <brantlauro@gmail.com>
2022-07-02 15:31:46 -0300

    :white_check_mark: ex003.js done

---
+++ mod1/ex-js/ex003.js
@@ -0,0 +1,24 @@
+
+// Aqui declarei a variável userName, usada para definir o nome do usuário
+let userName = "Lauro Brant";
+
+// Aqui usei a format string para facilitar a concatenação da variável dentro dessa String
+console.log(`Hello ${userName}, Welcome to JavaScript!`);
+
+// Aqui declarei duas variáveis na mesma linha, num1 e num2 depois declarei a variável sum que soma as duas variáveis
+let num1 = 3, num2 = 5;
+let sum = num1 + num2;
+
+// Aqui simplesmente mostrei o valor da soma, novamente utilizando format string
+console.log(`The sum is: ${sum}`);
+
+/*
+ ________________________________________
+/ Comentários apenas para cumprir a      \
+\ proposta, do exercício. ;)             /
+ ----------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     || */
