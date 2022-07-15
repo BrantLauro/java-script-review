aac84edcca9ad92b038ec26e19c0748466f330ec
BrantLauro <brantlauro@gmail.com>
2022-07-15 14:01:39 -0300

    :rocket: ex011.js done

---
+++ mod1/ex-js/ex011.js
@@ -0,0 +1,6 @@
+let wallWidth = 30;
+let wallHeight = 10;
+let area = wallWidth * wallHeight;
+let totalInk = area / 2;
+
+console.log(`To paint a ${wallWidth}x${wallHeight}m wall, that has ${area}m² of area, you will need ${totalInk} cans of ink.`);
