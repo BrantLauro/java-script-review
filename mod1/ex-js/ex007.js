be0eff1a77617c5c403094ecd5b34b59c6189655
BrantLauro <brantlauro@gmail.com>
2022-07-14 22:15:26 -0300

    :rocket: ex007.js done

---
+++ mod1/ex-js/ex007.js
@@ -0,0 +1,5 @@
+let meters = 5;
+let centimeters = meters * 100;
+let milimetres = meters * 1000;
+
+console.log(`${meters}m in centimeters are ${centimeters}cm, and in milimetres are ${milimetres}mm.`);
