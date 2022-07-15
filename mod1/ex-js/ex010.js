1c81e17a98e93a26a08009359c8dd590abb0beb4
BrantLauro <brantlauro@gmail.com>
2022-07-15 13:55:03 -0300

    :rocket: ex010.js done

---
+++ mod1/ex-js/ex010.js
@@ -0,0 +1,6 @@
+let reais = 90;
+let dollars = reais / 5.4;
+let euros = reais / 5.44;
+let yens = reais / 0.039;
+
+console.log(`With R$${reais.toFixed(2)} Reais you can buy $${dollars.toFixed(2)} Dollars, $${euros.toFixed(2)} Euros and $${yens.toFixed(2)} Yens.`);
