e935bff1568ab9eded3f0056ca4761d13ec0041f
BrantLauro <brantlauro@gmail.com>
2022-07-15 14:20:00 -0300

    :rocket: ex013.js done

---
+++ mod1/ex-js/ex013.js
@@ -0,0 +1,7 @@
+let grade1 = 2, grade2 = 3, grade3 = 10, grade4 = 1, average = ((grade1 + grade2 + grade3 + grade4) / 4);
+
+if (average >= 6) {
+    console.log(`Congratulations, you have passed with the average of ${average}`);
+} else {
+    console.log(`Oh, what a shame! You did not passed with the average of ${average}`);
+}
