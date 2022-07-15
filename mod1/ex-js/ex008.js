2849b5914184f9500755cc6cb1513968b97bb426
BrantLauro <brantlauro@gmail.com>
2022-07-14 22:21:04 -0300

    :rocket: ex008.js done

---
+++ mod1/ex-js/ex008.js
@@ -0,0 +1,13 @@
+let num = 5;
+
+console.log(`0 x ${num} = ${num * 0}
+1 x ${num} = ${num * 1}
+2 x ${num} = ${num * 2}
+3 x ${num} = ${num * 3}
+4 x ${num} = ${num * 4}
+5 x ${num} = ${num * 5}
+6 x ${num} = ${num * 6}
+7 x ${num} = ${num * 7}
+8 x ${num} = ${num * 8}
+9 x ${num} = ${num * 9}
+10 x ${num} = ${num * 10} `)
